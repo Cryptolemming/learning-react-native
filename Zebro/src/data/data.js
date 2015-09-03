a10182d64cd3d62b87fc0f73a09fb8448d4949bc
Bonnie Eisenman <bonnie@codecademy.com>
2015-09-03 17:08:20 -0400

    stub sql

--- Zebro/src/data/data.js
+++ Zebro/src/data/data.js
@@ -4,8 +4,38 @@ var Card = require('./Card');
 var SQLite = require('react-native-sqlite');
 
 
+class Database {
+  query(query, params, errorCB, rowCB, completeCB) {
+    SQLite.open('development.db', (error, database) => {
+      if (error) {
+        console.error('Failed to open  database: ', error);
+        errorCB(error);
+      }
+
+      function completeCallback(completeError) {
+        database.close((closeError) => {
+          console.error('Failed to close database: ', closeError);
+          errorCB(closeError);
+        });
+
+        if (completeError) {
+          console.error('Error from completeCallback:', completeError);
+          errorCB(completeError);
+        }
+
+        completeCB(completeError);
+      }
+
+      database.executeSQL(query, params, rowCB, completeCallback);
+    });
+  }
+}
+
 class Data {
   constructor() {
+
+    Database.query('SELECT * FROM decks', [], errorCB, rowCB, completeCB);
+
     var d = new Deck('Esperanto Vocabulary');
     this.decks = [
       d
